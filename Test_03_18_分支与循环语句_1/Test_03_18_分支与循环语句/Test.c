0c9900f2f96de440172ec00901ebd8b5adf5e87f
BranceCraker <1327968619@qq.com>
2020-03-19 13:34:33 +0800

    add a Test

--- Test_03_18_分支与循环语句_1/Test_03_18_分支与循环语句/Test.c
+++ Test_03_18_分支与循环语句_1/Test_03_18_分支与循环语句/Test.c
@@ -1,24 +1,202 @@
 #define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+
+//作业
+//8.打印100~200之间的素数
+
 #include<stdio.h>
+int main()
+{
+	for (int i = 100; i <= 200; ++i)
+	{
+		for (int j = 2; j < i; ++j)
+		{
+			if (i % j == 0)
+				break;
+		}
+		printf("%d\t", i);
+	}
+	return 0;
+}
+
+/*
+//7.打印1000年到2000年之间的闰年
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	for (int year = 1000; year <= 2000; ++year)
+	{
+		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+		{
+			printf("%d ", year);
+			++n;
+		}
+		if (n % 5 == 0)
+			printf("\n");
+	}
+	return 0;
+}
+
+
+//6.给定两个数，求这两个数的最大公约数
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	while (1)
+	{
+		int max = 0;
+		printf("please input two num:");
+		scanf("%d %d", &a, &b);
+		for (int i = 1; i < a; ++i)
+		{
+			if (a % i == 0 && b % i == 0)
+			{
+				printf("%d ", i);
+				max = i;
+			}
+		}
+		printf("\n最大公约数为:%d\n", max);
+	}
+	return 0;
+}
+
+
+//5.写一个代码打印1-100之间所有3的倍数的数字
+#include<stdio.h>
+int main()
+{
+	for (int i = 1; i < 100; ++i)
+	{
+		if (i % 3 == 0)
+		{
+			printf("%d ", i);
+		}
+	}
+	return 0;
+}
+
+//4.将三个数按从大到小输出
+#include<stdio.h>
+int Print(int x, int y, int z)
+{
+	if (x < y)
+	{
+		int tmp = x;
+		x = y;
+		y = tmp;
+	}
+	if (x < z)
+	{
+		int tmp = x;
+		x = z;
+		z = tmp;
+	}
+	if (y < z)
+	{
+		int tmp = y;
+		y = z;
+		z = tmp;
+	}
+	printf("从大到小:%d %d %d\n", x, y, z);
+	return 0;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	while (1)
+	{
+		printf("please input three num:");
+		scanf("%d %d %d", &a, &b, &c);
+		Print(a, b, c);
+	}
+	return 0;
+}
+
+/*
+//3.
+#include<stdio.h>
+int main()
+{
+	int x = 3;
+	int y = 3;
+	switch (x % 2)
+	{
+	case 1:
+		switch (y)
+		{
+		case 0:
+			printf("first");
+		case 1:
+			printf("second");
+			break;
+		default: printf("hello");
+		}
+	case 2:
+		printf("third");
+	}
+	return 0;
+}
+
+//2.
+#include<stdio.h>
+int func(int a)
+{
+	int b;
+	switch (a)
+	{
+	case 1: b = 30;
+	case 2: b = 20;
+	case 3: b = 16;
+	default: b = 0;
+	}
+	return b;
+}
+int main()
+{
+	int x = func(1);
+	printf("%d ", x);
+	return 0;
+}
+
+
+//1.
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		if (i = 5)
+			printf("%d ", i);
+	}
+	return 0;
+}
+
+
 
 int main()
 {
 	int i = 0;
 //while循环中的continue，跳过本次循环后边的代码直接去判断部分，
 //进行下一项入口判断
-//其实在循环中只要遇到break，就停止后期的所有的循环，直接终止循环。 
+//其实在循环中只要遇到break，就停止后期的所有的循环，直接终止循环。
 //所以：while中的break是用于永久终止循环的
 	while (i < 10)
 	{
 		if (i == 5)
-			continue; 
+			continue;
 		printf("%d ", i);
 		i++;
 	}
 	return 0;
 }
 
-/*
+
 int main()
 {
 	int n = 1;
@@ -105,7 +283,7 @@ int main()
 //	}
 //}
 
-	
+
 	//判断数字是不是奇数
 	int main()
 	{
